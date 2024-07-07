95d94e31fe887b540b694c60b67a4a61a49518e0
Tom Wiesing <tkw01536@gmail.com>
2024-07-07 13:25:45 +0200

    disclaimer: Don't pipe stderr to outpu

--- macros/disclaimer.ts
+++ macros/disclaimer.ts
@@ -2,20 +2,30 @@ import {
   type CompileTimeFunctionArgs,
   type CompileTimeFunctionResult
 } from 'vite-plugin-compile-time'
-import { execSync } from 'child_process'
+import { exec } from 'child_process'
 import { join } from 'path'
 
 const PACKAGE_JSON = join(__dirname, '..', 'package.json')
 const YARN_LOCK = join(__dirname, '..', 'yarn.lock')
 
 /** generates a legal disclaimer to include */
-export default function generateDisclaimer (args: CompileTimeFunctionArgs): CompileTimeFunctionResult { // eslint-disable-line @typescript-eslint/no-invalid-void-type
-  const buffer = execSync('yarn licenses generate-disclaimer --prod')
+export default async function generateDisclaimer (args: CompileTimeFunctionArgs): DefinitelyPromise<CompileTimeFunctionResult> { // eslint-disable-line @typescript-eslint/no-invalid-void-type
+  const buffer = await new Promise(
+    (resolve, reject) => exec('yarn licenses generate-disclaimer --prod', (error, stdout, stderr) => {
+      if (error !== null) {
+        reject(error)
+        return
+      }
+      resolve(stdout)
+    })
+  )
   return {
     watchFiles: [
       PACKAGE_JSON,
       YARN_LOCK
     ],
-    data: buffer.toString()
+    data: buffer
   }
 }
+
+type DefinitelyPromise<T> = T extends (Promise<infer X> | infer X) ? Promise<X> : never
