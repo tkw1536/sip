1b80aff58b7b0f3226bd69b979361ad40ef8d74f
Tom Wiesing <tkw01536@gmail.com>
2024-07-07 14:36:16 +0200

    migrate to @prefresh/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vitest" />
-import { defineConfig } from "vite"
-import compileTime from "vite-plugin-compile-time"
-import preact from "@preact/preset-vite"
+import { defineConfig } from 'vite'
+import compileTime from 'vite-plugin-compile-time'
+import prefresh from '@prefresh/vite'
 
 export default defineConfig({
   plugins: [
-    preact({ reactAliasesEnabled: false }),
-    compileTime(),
+    prefresh(),
+    compileTime()
   ],
   worker: {
     format: 'es'
   },
   test: {
     environment: 'happy-dom'
-  },
+  }
 })
