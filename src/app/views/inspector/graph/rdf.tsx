780b726abafcc286da303973097547eb6ea702f1
Tom Wiesing <tkw01536@gmail.com>
2024-07-12 01:57:09 +0200

    RDF: allow customizing namespace map

--- src/app/views/inspector/graph/rdf.tsx
+++ src/app/views/inspector/graph/rdf.tsx
@@ -14,52 +14,85 @@ import RDFGraphBuilder, {
 import type Graph from '../../../../lib/graph'
 import DropArea from '../../../../lib/components/drop-area'
 import * as styles from './rdf.module.css'
-import { graph, parse } from 'rdflib'
+import { graph, parse, type Store } from 'rdflib'
+import NamespaceEditor from '../../../../lib/components/namespace-editor'
+import { NamespaceMap } from '../../../../lib/pathbuilder/namespace'
+import { Operation } from '../../../../lib/utils/operation'
+import ErrorDisplay from '../../../../lib/components/error'
+import { type Term } from 'rdflib/lib/tf-types'
 
 export default class RDFGraphView extends Component<
   ReducerProps,
-  { data?: Blob }
+  { loadError?: any; store?: Store; fileName?: string }
 > {
-  state: { data?: File } = {}
-  readonly #builder = async (): Promise<Graph<RDFNode, RDFEdge>> => {
-    const { data } = this.state
-    if (typeof data === 'undefined')
-      throw new Error('tried to build a graph, but there is no blob')
+  state: { loadError?: any; store?: Store; fileName?: string } = {}
 
-    const text = await data.text()
+  readonly #handleClose = (): void => {
+    this.setState({ store: undefined, fileName: undefined })
+  }
+
+  readonly #operation = new Operation()
+  readonly #handleOpen = (file: File): void => {
+    const ticket = this.#operation.ticket()
+    this.#loadFile(file).then(
+      ({ store, fileName }) => {
+        this.setState(() => {
+          if (!ticket()) return null
+          return { store, fileName, loadError: undefined }
+        })
+      },
+      loadError => {
+        this.setState(() => {
+          if (!ticket()) return null
+          return { store: undefined, fileName: undefined, loadError }
+        })
+      },
+    )
+    void file.text().then(text => {
+      this.setState(() => {
+        if (!ticket()) return null
+        return { fileText: text, fileName: file.name }
+      })
+    })
+  }
+
+  async #loadFile(file: File): Promise<{ store: Store; fileName: string }> {
+    const text = await file.text()
+    const { name } = file
+
     const format = 'application/rdf+xml'
-    const { name } = data
     const base = 'file://' + (name !== '' ? name : 'upload.xml') // TODO: allow user to set this
 
     // parse in the graph
     const store = graph()
     parse(text, store, base, format)
 
-    // create a builder and get the text ready
-    const builder = new RDFGraphBuilder(store.close())
-    return await builder.build()
+    return { store: store.close(), fileName: name }
   }
 
-  readonly #handleClose = (evt: Event): void => {
-    evt.preventDefault()
-    this.setState({ data: undefined })
+  componentWillUnmount(): void {
+    this.#operation.cancel()
   }
-
-  readonly #handleOpen = (file: File): void => {
-    this.setState({ data: file })
-  }
-
-  readonly #displayRef = createRef<GraphDisplay<RDFNode, RDFEdge>>()
 
   render(): ComponentChildren {
-    const { data } = this.state
-    if (typeof data === 'undefined') {
-      return this.renderLoad()
-    }
-    return this.renderView()
-  }
+    const { store, fileName, loadError } = this.state
 
-  renderLoad(): ComponentChildren {
+    // file loaded => display it
+    if (
+      typeof loadError === 'undefined' &&
+      typeof store !== 'undefined' &&
+      typeof fileName !== 'undefined'
+    ) {
+      return (
+        <RDFGraphDisplay
+          store={store}
+          fileName={fileName}
+          onClose={this.#handleClose}
+          {...this.props}
+        />
+      )
+    }
+
     return (
       <>
         <h2>RDF Visualizer</h2>
@@ -75,34 +108,49 @@ export default class RDFGraphView extends Component<
         >
           Drop <code>RDF/XML</code> here
         </DropArea>
+        {typeof loadError !== 'undefined' && <ErrorDisplay error={loadError} />}
       </>
     )
   }
+}
 
-  renderView(): ComponentChildren {
-    const {
-      rdfGraphLayout,
-      rdfGraphDriver,
-      pathbuilderVersion,
-      selectionVersion,
-      colorVersion,
-      ns,
-      cm,
-    } = this.props.state
+interface GraphDisplayState {
+  ns: NamespaceMap
+  nsKey: number
+}
 
-    return (
-      <GraphDisplay
-        ref={this.#displayRef}
-        loader={triples}
-        driver={rdfGraphDriver}
-        builderKey={`${pathbuilderVersion}-${selectionVersion}-${colorVersion}`}
-        makeGraph={this.#builder}
-        ns={ns}
-        cm={cm}
-        layout={rdfGraphLayout}
-        panel={this.#renderPanel}
-      />
-    )
+class RDFGraphDisplay extends Component<
+  {
+    store: Store
+    fileName: string
+    onClose: () => void
+  } & ReducerProps,
+  GraphDisplayState
+> {
+  readonly #displayRef = createRef<GraphDisplay<RDFNode, RDFEdge>>()
+
+  /** generates a namespace map from the props */
+  readonly #generateNS = (): NamespaceMap => {
+    const uris = new Set<string>()
+    const addTerm = (term: Term): void => {
+      if (term.termType !== 'NamedNode') return
+      uris.add(term.value)
+    }
+    this.props.store.statements.forEach(statement => {
+      addTerm(statement.subject)
+      addTerm(statement.predicate)
+      addTerm(statement.object)
+    })
+    return NamespaceMap.generate(uris)
+  }
+  state: GraphDisplayState = {
+    ns: this.#generateNS(),
+    nsKey: 0,
+  }
+
+  readonly buildGraph = async (): Promise<Graph<RDFNode, RDFEdge>> => {
+    const builder = new RDFGraphBuilder(this.props.store)
+    return await builder.build()
   }
 
   readonly #handleChangeRDFRenderer = (value: string): void => {
@@ -113,12 +161,47 @@ export default class RDFGraphView extends Component<
     this.props.apply(setRDFLayout(value))
   }
 
+  render(): ComponentChildren {
+    const { rdfGraphLayout, rdfGraphDriver, cm } = this.props.state
+
+    const { ns, nsKey } = this.state
+
+    return (
+      <GraphDisplay
+        ref={this.#displayRef}
+        loader={triples}
+        driver={rdfGraphDriver}
+        builderKey={nsKey.toString()}
+        makeGraph={this.buildGraph}
+        ns={ns}
+        cm={cm}
+        layout={rdfGraphLayout}
+        panel={this.#renderPanel}
+      />
+    )
+  }
+
+  readonly #handleResetNS = (): void => {
+    this.setState(({ nsKey }) => ({
+      ns: this.#generateNS(),
+      nsKey: nsKey + 1,
+    }))
+  }
+  readonly #handleSetNS = (ns: NamespaceMap): void => {
+    this.setState(({ nsKey }) => ({
+      ns,
+      nsKey: nsKey + 1,
+    }))
+  }
+
   readonly #renderPanel = (
     driver: Driver<RDFNode, RDFEdge> | null,
   ): ComponentChildren => {
     const {
       state: { rdfGraphLayout },
     } = this.props
+
+    const { ns, nsKey } = this.state
 
     return (
       <>
@@ -129,8 +212,16 @@ export default class RDFGraphView extends Component<
           onChangeDriver={this.#handleChangeRDFRenderer}
           onChangeLayout={this.#handleChangeRDFLayout}
         />
+        <Control name='Namespace Map'>
+          <NamespaceEditor
+            ns={ns}
+            nsKey={nsKey}
+            onUpdate={this.#handleSetNS}
+            onReset={this.#handleResetNS}
+          ></NamespaceEditor>
+        </Control>
         <Control name='Close'>
-          <button onClick={this.#handleClose}>Close and Open Another</button>
+          <button onClick={this.props.onClose}>Close and Open Another</button>
         </Control>
         <ExportControl driver={driver} display={this.#displayRef.current} />
       </>
