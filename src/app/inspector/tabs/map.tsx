5fc87ab5e4a19c604166bc009f73b9e50b88c706
Tom Wiesing <tkw01536@gmail.com>
2024-07-23 18:26:29 +0200

    inspector/tabs/namespaceMap: Use SFC

--- src/app/inspector/tabs/map.tsx
+++ src/app/inspector/tabs/map.tsx
@@ -1,35 +1,39 @@
-import { Component, type ComponentChild } from 'preact'
+import { type JSX } from 'preact'
 import { type IReducerProps } from '../state'
 import { resetNamespaceMap, setNamespaceMap } from '../state/reducers/ns'
 import { type NamespaceMap } from '../../../lib/pathbuilder/namespace'
 import NamespaceEditor from '../../../components/namespace-editor'
+import { useCallback } from 'preact/hooks'
 
-export default class MapTab extends Component<IReducerProps> {
-  readonly #handleReset = (): void => {
-    this.props.apply(resetNamespaceMap())
-  }
-  readonly #handleNewMap = (ns: NamespaceMap): void => {
-    this.props.apply(setNamespaceMap(ns))
-  }
-  render(): ComponentChild {
-    return (
-      <>
-        <p>
-          The Namespace Map is used to shorten URIs for display within the
-          inspector. The underlying pathbuilder always contains the full URIs,
-          and namespaces are not saved across reloads.
-          <br />
-          The initial version is generated automatically from all URIs found in
-          the pathbuilder. You can manually adjust it here, by adding, removing
-          or editing abbreviations.
-        </p>
-        <NamespaceEditor
-          ns={this.props.state.ns}
-          nsKey={this.props.state.namespaceVersion}
-          onReset={this.#handleReset}
-          onUpdate={this.#handleNewMap}
-        />
-      </>
-    )
-  }
+export default function MapTab(props: IReducerProps): JSX.Element {
+  const handleReset = useCallback((): void => {
+    props.apply(resetNamespaceMap())
+  }, [props.apply, resetNamespaceMap])
+
+  const handleNewMap = useCallback(
+    (ns: NamespaceMap): void => {
+      props.apply(setNamespaceMap(ns))
+    },
+    [props.apply, setNamespaceMap],
+  )
+
+  return (
+    <>
+      <p>
+        The Namespace Map is used to shorten URIs for display within the
+        inspector. The underlying pathbuilder always contains the full URIs, and
+        namespaces are not saved across reloads.
+        <br />
+        The initial version is generated automatically from all URIs found in
+        the pathbuilder. You can manually adjust it here, by adding, removing or
+        editing abbreviations.
+      </p>
+      <NamespaceEditor
+        ns={props.state.ns}
+        nsKey={props.state.namespaceVersion}
+        onReset={handleReset}
+        onUpdate={handleNewMap}
+      />
+    </>
+  )
 }
