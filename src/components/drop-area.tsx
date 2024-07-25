744ae747da9d78695cfaa91be71436beaa2ac8bf
Tom Wiesing <tkw01536@gmail.com>
2024-07-25 10:48:54 +0200

    drop-area: Move to functional component

--- src/components/drop-area.tsx
+++ src/components/drop-area.tsx
@@ -1,12 +1,7 @@
-import {
-  Component,
-  type ComponentChild,
-  type JSX,
-  type VNode,
-  createRef,
-} from 'preact'
+import { type ComponentChild, type JSX, type VNode } from 'preact'
 import { classes } from '../lib/utils/classes'
 import * as styles from './drop-area.module.css'
+import { useCallback, useMemo, useRef, useState } from 'preact/hooks'
 
 interface DropAreaProps {
   onDropFile: (...files: File[]) => void
@@ -26,143 +21,170 @@ interface DropAreaProps {
     | ((active: boolean, valid: boolean) => ComponentChild)
 }
 
-export default class DropArea extends Component<DropAreaProps> {
-  readonly #callFileHandler = (files?: FileList): void => {
-    if (!this.#validateFiles(files)) return
-    this.props.onDropFile(...Array.from(files))
+/**
+ * Checks if files pass the given conditions
+ *
+ * @param types the types of files allowed, or undefined if all types should be permitted
+ * @param multiple if multiple files are permitted
+ * @param empty if no files are permitted
+ * @param items the items to validate
+ * @returns
+ */
+function validateFiles(
+  types: string[] | undefined,
+  multiple: boolean,
+  empty: boolean = false,
+  items:
+    | ArrayLike<{ readonly type: string; readonly kind?: string }>
+    | undefined,
+): items is ArrayLike<{ readonly type: string }> {
+  // check that we have the right number of elements
+  if (typeof items === 'undefined' || items.length === 0) return empty
+  if (!(multiple ?? false) && items.length > 1) return false
+
+  // check each item
+  const allowed = new Set(types ?? [])
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i]
+
+    // kind === 'file' (skipped if already a file)
+    if (typeof item.kind === 'string' && item.kind !== 'file') return false
+
+    // check against type list (skipped if not allowed)
+    if (allowed.size > 0 && !allowed.has(item.type)) return false
   }
 
-  /** validateFiles checks if the given items or files are valid */
-  readonly #validateFiles = (
-    items?: ArrayLike<{ readonly type: string; readonly kind?: string }>,
-    allowEmpty: boolean = false,
-  ): items is ArrayLike<{ readonly type: string }> => {
-    const { multiple, types } = this.props
+  // everything is valid!
+  return true
+}
 
-    // check that we have the right number of elements
-    if (typeof items === 'undefined' || items.length === 0) return allowEmpty
-    if (!(multiple ?? false) && items.length > 1) return false
+export default function DropArea(props: DropAreaProps): JSX.Element {
+  const {
+    class: clz,
+    activeInvalidClass,
+    activeValidClass,
+    passiveClass,
+    children,
+    types,
+    multiple,
+    compact,
+    onDropFile,
+  } = props
 
-    // check each item
-    const allowed = new Set(types ?? [])
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i]
+  const [dragActive, setDragActive] = useState(false)
+  const [dragValid, setDragValid] = useState(false)
 
-      // kind === 'file' (skipped if already a file)
-      if (typeof item.kind === 'string' && item.kind !== 'file') return false
+  const callFileHandler = useMemo(
+    () =>
+      (files?: FileList): void => {
+        if (!validateFiles(types, multiple ?? false, false, files)) return
+        onDropFile(...Array.from(files))
+      },
+    [multiple, onDropFile, types],
+  )
 
-      // check against type list (skipped if not allowed)
-      if (allowed.size > 0 && !allowed.has(item.type)) return false
-    }
+  const handleDropFile = useCallback(
+    (event: DragEvent): void => {
+      event.preventDefault()
 
-    // everything is valid!
-    return true
-  }
+      setDragActive(false)
+      callFileHandler(event.dataTransfer?.files)
+    },
+    [callFileHandler],
+  )
 
-  state = { dragActive: false, dragValid: false }
+  const handleDropOver = useCallback(
+    (event: DragEvent): void => {
+      event.preventDefault()
 
-  readonly #handleDropFile = (event: DragEvent): void => {
+      setDragActive(true)
+      setDragValid(
+        validateFiles(
+          types,
+          multiple ?? false,
+          true,
+          event.dataTransfer?.items,
+        ),
+      )
+    },
+    [multiple, types],
+  )
+
+  const handleDragLeave = useCallback((event: DragEvent): void => {
     event.preventDefault()
-    this.setState({ dragActive: false })
+    setDragActive(false)
+    setDragValid(false)
+  }, [])
 
-    this.#callFileHandler(event.dataTransfer?.files)
-  }
+  const fileInput = useRef<HTMLInputElement>(null)
 
-  readonly #handleDropOver = (event: DragEvent): void => {
-    event.preventDefault()
-    this.setState({
-      dragActive: true,
-      dragValid: this.#validateFiles(event.dataTransfer?.items, true),
-    })
-  }
-
-  readonly #handleDragLeave = (event: DragEvent): void => {
-    event.preventDefault()
-    this.setState({ dragActive: false, dragValid: false })
-  }
-
-  readonly #handleClick = (event: MouseEvent): void => {
+  const handleClick = useCallback((event: MouseEvent): void => {
     event.preventDefault()
 
-    const { current } = this.#fileInput
+    const { current } = fileInput
     if (current === null) return
     current.click()
+  }, [])
+
+  const handleUploadFile = useCallback(
+    (event: Event): void => {
+      event.preventDefault()
+
+      callFileHandler(fileInput?.current?.files ?? undefined)
+    },
+    [callFileHandler],
+  )
+
+  // determine classes to apply
+  const dropClasses = [clz]
+  switch (true) {
+    case dragActive && dragValid:
+      dropClasses.push(activeValidClass)
+      break
+    case dragActive && !dragValid:
+      dropClasses.push(activeInvalidClass ?? activeValidClass)
+      break
+    default:
+      dropClasses.push(passiveClass)
   }
 
-  readonly #handleUploadFile = (event: Event): void => {
-    event.preventDefault()
+  const childNodes =
+    typeof children === 'function' ? children(dragActive, dragValid) : children
 
-    this.#callFileHandler(this.#fileInput?.current?.files ?? undefined)
-  }
-
-  readonly #fileInput = createRef<HTMLInputElement>()
-
-  render(): ComponentChild {
-    const { dragActive, dragValid } = this.state
-    const {
-      class: clz,
-      activeInvalidClass,
-      activeValidClass,
-      passiveClass,
-      children,
-      multiple,
-      types,
-      compact,
-    } = this.props
-
-    // determine classes to apply
-    const dropClasses = [clz]
-    switch (true) {
-      case dragActive && dragValid:
-        dropClasses.push(activeValidClass)
-        break
-      case dragActive && !dragValid:
-        dropClasses.push(activeInvalidClass ?? activeValidClass)
-        break
-      default:
-        dropClasses.push(passiveClass)
-    }
-
-    const childNodes =
-      typeof children === 'function'
-        ? children(dragActive, dragValid)
-        : children
-
-    let main: VNode
-    if (!(compact ?? false)) {
-      main = (
-        <div
-          class={classes(...dropClasses)}
-          onDrop={this.#handleDropFile}
-          onDragOver={this.#handleDropOver}
-          onDragLeave={this.#handleDragLeave}
-          onClick={this.#handleClick}
-        >
-          {childNodes}
-        </div>
-      )
-    } else {
-      main = (
-        <button class={classes(clz)} onClick={this.#handleClick}>
-          {childNodes}
-        </button>
-      )
-    }
-
-    return (
-      <>
-        <input
-          type='file'
-          style={{ display: 'none' }}
-          ref={this.#fileInput}
-          multiple={multiple}
-          accept={types?.join(',')}
-          onInput={this.#handleUploadFile}
-        />
-        {main}
-      </>
+  let main: VNode
+  if (!(compact ?? false)) {
+    main = (
+      <div
+        class={classes(...dropClasses)}
+        onDrop={handleDropFile}
+        onDragOver={handleDropOver}
+        onDragLeave={handleDragLeave}
+        onClick={handleClick}
+      >
+        {childNodes}
+      </div>
+    )
+  } else {
+    main = (
+      <button class={classes(clz)} onClick={handleClick}>
+        {childNodes}
+      </button>
     )
   }
+
+  return (
+    <>
+      <input
+        type='file'
+        style={{ display: 'none' }}
+        ref={fileInput}
+        multiple={multiple}
+        accept={types?.join(',')}
+        onInput={handleUploadFile}
+      />
+      {main}
+    </>
+  )
 }
 
 export function StyledDropArea(
