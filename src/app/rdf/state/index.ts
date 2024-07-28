94331f90c8ecb9cc7eeeea4862228d04bf968553
Tom Wiesing <tkw01536@gmail.com>
2024-07-28 13:44:59 +0200

    rdf/state: Update import syntax

--- src/app/rdf/state/index.ts
+++ src/app/rdf/state/index.ts
@@ -1,14 +1,18 @@
 import { create } from 'zustand'
-import { create as createFile, type Slice as FileSlice } from './file'
-import { create as createModal, type Slice as ModalSlice } from './modal'
-import { create as createNS, type Slice as NSSlice } from './ns'
-import { create as createRDF, type Slice as RDFSlice } from './rdf'
-import { create as createTab, type Slice as TabSlice } from './tab'
+import * as File from './file'
+import * as Modal from './modal'
+import * as NS from './ns'
+import * as RDF from './rdf'
+import * as Tab from './tab'
 
 import { type Store } from 'rdflib'
 
 /** the set of all states */
-export type BoundState = FileSlice & ModalSlice & NSSlice & TabSlice & RDFSlice
+export type BoundState = File.Slice &
+  Modal.Slice &
+  NS.Slice &
+  Tab.Slice &
+  RDF.Slice
 
 /** contains all functions that reset the state */
 export const resetters = new Set<() => void>()
@@ -18,11 +22,11 @@ export const loaders = new Set<(store: Store) => Promise<Partial<BoundState>>>()
 
 /** the store for rdf */
 const useRDFStore = create<BoundState>()((...a) => ({
-  ...createFile(...a),
-  ...createModal(...a),
-  ...createNS(...a),
-  ...createRDF(...a),
-  ...createTab(...a),
+  ...File.create(...a),
+  ...Modal.create(...a),
+  ...NS.create(...a),
+  ...RDF.create(...a),
+  ...Tab.create(...a),
 }))
 
 export default useRDFStore
